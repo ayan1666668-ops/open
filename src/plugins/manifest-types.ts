13972485ef3d867cda8483caf2bf1b946f4ffaf3
NewdlDewdl <rohin.agrawal@gmail.com>
2026-07-25 22:29:33 -0500

    fix(plugins): keep reload metadata helper internal

--- src/plugins/manifest-types.ts
+++ src/plugins/manifest-types.ts
@@ -64,7 +64,7 @@ export type PluginManifestChannelConfig = {
   commands?: PluginManifestChannelCommandDefaults;
 };
 
-export type PluginManifestChannelReload = {
+type PluginManifestChannelReload = {
   configPrefixes?: string[];
   noopPrefixes?: string[];
   accountIndexReloadPaths?: string[];
