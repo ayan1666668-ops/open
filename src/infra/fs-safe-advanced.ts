389f4287f2e531335d03b51edd180a1be2283132
Peter Steinberger <steipete@gmail.com>
2026-09-14 09:02:34 -0700

    refactor: reuse fs-safe filesystem owners

--- src/infra/fs-safe-advanced.ts
+++ src/infra/fs-safe-advanced.ts
@@ -5,6 +5,7 @@ import "./fs-safe-defaults.js";
 export {
   assertNoSymlinkParents,
   assertNoSymlinkParentsSync,
+  probePathCaseInsensitiveSync,
   readFileHandleBounded,
   type FileIdentityStat,
   sameFileIdentity,
