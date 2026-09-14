clawdbot/clawdbot

feat(capability): add private model-run prompt files

apps/shared/OpenClawKit/Sources/OpenClawProtocol/GatewayModels.swift
@@ -2659,6 +2659,7 @@ public struct AgentParams: Codable, Sendable {
     public let cwd: String?
     public let cleanupbundlemcponrunend: Bool?
     public let modelrun: Bool?
+    public let modelrunoptions: [String: AnyCodable]?
     public let promptmode: AnyCodable?
     public let extrasystemprompt: String?
     public let bootstrapcontextmode: AnyCodable?
@@ -2708,6 +2709,7 @@ public struct AgentParams: Codable, Sendable {
         cwd: String? = nil,
         cleanupbundlemcponrunend: Bool? = nil,
         modelrun: Bool? = nil,
+        modelrunoptions: [String: AnyCodable]? = nil,
         promptmode: AnyCodable? = nil,
         extrasystemprompt: String? = nil,
         bootstrapcontextmode: AnyCodable? = nil,
@@ -2756,6 +2758,7 @@ public struct AgentParams: Codable, Sendable {
         self.cwd = cwd
         self.cleanupbundlemcponrunend = cleanupbundlemcponrunend
         self.modelrun = modelrun
+        self.modelrunoptions = modelrunoptions
         self.promptmode = promptmode
         self.extrasystemprompt = extrasystemprompt
         self.bootstrapcontextmode = bootstrapcontextmode
@@ -2806,6 +2809,7 @@ public struct AgentParams: Codable, Sendable {
         case cwd
         case cleanupbundlemcponrunend = "cleanupBundleMcpOnRunEnd"
         case modelrun = "modelRun"
+        case modelrunoptions = "modelRunOptions"
         case promptmode = "promptMode"
         case extrasystemprompt = "extraSystemPrompt"
         case bootstrapcontextmode = "bootstrapContextMode"

packages/gateway-protocol/src/schema/agent.ts
@@ -308,6 +308,12 @@ export const AgentParamsSchema = closedObject({
   // bundle MCP resources after the run instead of keeping them warm.
   cleanupBundleMcpOnRunEnd: Type.Optional(Type.Boolean()),
   modelRun: Type.Optional(Type.Boolean()),
+  modelRunOptions: Type.Optional(
+    closedObject({
+      maxTokens: Type.Optional(Type.Integer({ minimum: 1, maximum: 1_000_000 })),
+      temperature: Type.Optional(Type.Number({ minimum: 0, maximum: 2 })),
+    }),
+  ),
   promptMode: Type.Optional(
     Type.Union([Type.Literal("full"), Type.Literal("minimal"), Type.Literal("none")]),
   ),

src/cli/capability-cli.test.ts
@@ -648,7 +648,7 @@ describe("capability cli", () => {
       messages?: Array<{ content?: unknown; role?: unknown }>;
       systemPrompt?: unknown;
     };
-    options?: { reasoning?: unknown };
+    options?: { maxTokens?: unknown; reasoning?: unknown; temperature?: unknown };
   };
   type ImageDescribeParams = {
     filePath?: string;
@@ -938,6 +938,143 @@ describe("capability cli", () => {
     await runCapability("model", "run", "--prompt", "hello", "--thinking", "high", "--json");
 
     expect(firstCompletionCall()?.options?.reasoning).toBe("high");
+  });
+
+  it("reads private model prompts from a mode-0600 prompt file", async () => {
+    const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), "openclaw-model-run-prompt-"));
+    const promptFile = path.join(tempDir, "prompt.txt");
+    await fs.writeFile(promptFile, "private prompt", { mode: 0o600 });
+
+    try {
+      await runCap("capability", "model", "run", "--prompt-file", promptFile, "--json");
+
+      expect(firstCompletionCall()?.context?.messages?.[0]?.content).toBe("private prompt");
+    } finally {
+      await fs.rm(tempDir, { recursive: true, force: true });
+    }
+  });
+
+  it("rejects prompt text combined with a prompt file", async () => {
+    const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), "openclaw-model-run-prompt-"));
+    const promptFile = path.join(tempDir, "prompt.txt");
+    await fs.writeFile(promptFile, "private prompt", { mode: 0o600 });
+
+    try {
+      await expect(
+        runCap(
+          "capability",
+          "model",
+          "run",
+          "--prompt",
+          "argv prompt",
+          "--prompt-file",
+          promptFile,
+          "--json",
+        ),
+      ).rejects.toThrow("exit 1");
+
+      expectRuntimeErrorContains("Use exactly one of --prompt or --prompt-file.");
+      expect(mocks.completeWithPreparedSimpleCompletionModel).not.toHaveBeenCalled();
+      expect(mocks.callGateway).not.toHaveBeenCalled();
+    } finally {
+      await fs.rm(tempDir, { recursive: true, force: true });
+    }
+  });
+
+  it.each([
+    {
+      name: "group-readable file",
+      setup: async (tempDir: string) => {
+        const promptFile = path.join(tempDir, "prompt.txt");
+        await fs.writeFile(promptFile, "private prompt", { mode: 0o600 });
+        await fs.chmod(promptFile, 0o640);
+        return promptFile;
+      },
+      expected: "must have mode 0600",
+    },
+    {
+      name: "symbolic link",
+      setup: async (tempDir: string) => {
+        const target = path.join(tempDir, "target.txt");
+        const promptFile = path.join(tempDir, "prompt.txt");
+        await fs.writeFile(target, "private prompt", { mode: 0o600 });
+        await fs.symlink(target, promptFile);
+        return promptFile;
+      },
+      expected: "must not be a symbolic link",
+    },
+    {
+      name: "empty file",
+      setup: async (tempDir: string) => {
+        const promptFile = path.join(tempDir, "prompt.txt");
+        await fs.writeFile(promptFile, "", { mode: 0o600 });
+        return promptFile;
+      },
+      expected: "cannot be empty",
+    },
+    {
+      name: "oversized file",
+      setup: async (tempDir: string) => {
+        const promptFile = path.join(tempDir, "prompt.txt");
+        await fs.writeFile(promptFile, Buffer.alloc(1024 * 1024 + 1, 0x61), { mode: 0o600 });
+        return promptFile;
+      },
+      expected: "must not exceed 1048576 bytes",
+    },
+  ])("rejects an unsafe $name prompt input", async ({ setup, expected }) => {
+    const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), "openclaw-model-run-prompt-"));
+    try {
+      const promptFile = await setup(tempDir);
+      await expect(
+        runCap("capability", "model", "run", "--prompt-file", promptFile, "--json"),
+      ).rejects.toThrow("exit 1");
+
+      expectRuntimeErrorContains(expected);
+      expect(mocks.completeWithPreparedSimpleCompletionModel).not.toHaveBeenCalled();
+      expect(mocks.callGateway).not.toHaveBeenCalled();
+    } finally {
+      await fs.rm(tempDir, { recursive: true, force: true });
+    }
+  });
+
+  it("freezes and reports effective local model-run settings", async () => {
+    await runCap(
+      "capability",
+      "model",
+      "run",
+      "--prompt",
+      "hello",
+      "--max-output-tokens",
+      "64",
+      "--temperature",
+      "0",
+      "--json",
+    );
+
+    expect(firstCompletionCall()?.options).toMatchObject({ maxTokens: 64, temperature: 0 });
+    expect(firstJsonOutput()?.effectiveSettings).toEqual({ maxOutputTokens: 64, temperature: 0 });
+  });
+
+  it("propagates and reports effective gateway model-run settings", async () => {
+    await runCap(
+      "capability",
+      "model",
+      "run",
+      "--prompt",
+      "hello",
+      "--gateway",
+      "--max-output-tokens",
+      "64",
+      "--temperature",
+      "0",
+      "--json",
+    );
+
+    expect(firstGatewayCall()?.params?.modelRunOptions).toEqual({
+      maxTokens: 64,
+      temperature: 0,
+    });
+    expect(firstJsonOutput()?.effectiveSettings).toEqual({ maxOutputTokens: 64, temperature: 0 });
   });
 
   it("passes image files to gateway model probes as attachments", async () => {

src/gateway/server-methods/agent-request-preflight.ts
@@ -196,6 +196,14 @@ export function prepareAgentRequestPreflight(
     );
     return undefined;
   }
+  if (request.modelRunOptions !== undefined && !isOneShotModelRun) {
+    params.respond(
+      false,
+      undefined,
+      errorShape(ErrorCodes.INVALID_REQUEST, "modelRunOptions requires modelRun=true."),
+    );
+    return undefined;
+  }
   if (requestedModelOverride && !allowModelOverride) {
     params.respond(
       false,

src/gateway/server-methods/agent.events-and-subagents.test-utils.ts
@@ -486,6 +486,7 @@ describe("gateway agent handler", () => {
         provider: "ollama",
         model: "llama3.2:latest",
         modelRun: true,
+        modelRunOptions: { maxTokens: 64, temperature: 0 },
         promptMode: "none",
         sessionKey: "agent:main:main",
         inputProvenance: {
@@ -504,6 +505,7 @@ describe("gateway agent handler", () => {
     const callArgs = await waitForAgentCommandCall<{
       message?: string;
       modelRun?: boolean;
+      streamParams?: { maxTokens?: number; temperature?: number };
       promptMode?: string;
     }>();
     expectRecordFields(callArgs, {
@@ -511,9 +513,32 @@ describe("gateway agent handler", () => {
       modelRun: true,
       promptMode: "none",
     });
+    expect(callArgs.streamParams).toEqual({ maxTokens: 64, temperature: 0 });
     expect(callArgs.message).not.toContain("[Inter-session message]");
 
     resetTimeConfig();
+  });
+
+  it("rejects model-run generation settings outside model-run requests", async () => {
+    primeMainAgentRun({ cfg: mocks.loadConfigReturn });
+    mocks.agentCommand.mockClear();
+
+    const respond = await invokeAgent(
+      {
+        message: "unsafe settings",
+        agentId: "main",
+        sessionKey: "agent:main:main",
+        modelRunOptions: { maxTokens: 64, temperature: 0 },
+        idempotencyKey: "test-model-run-options-without-model-run",
+      },
+      { reqId: "model-run-options-without-model-run", flushDispatch: false },
+    );
+
+    expectRespondError(respond, {
+      code: ErrorCodes.INVALID_REQUEST,
+      message: "modelRunOptions requires modelRun=true.",
+    });
+    expect(mocks.agentCommand).not.toHaveBeenCalled();
   });
 
   it("rejects promptMode none without the stateless model-run contract", async () => {

ayan1666668-ops/open

apps/shared/OpenClawKit/Sources/OpenClawProtocol/GatewayModels.swift
@@ -464,6 +464,7 @@ public struct AgentParams: Codable, Sendable {
     public let cwd: String?
     public let cleanupbundlemcponrunend: Bool?
     public let modelrun: Bool?
+    public let modelrunoptions: [String: AnyCodable]?
     public let promptmode: AnyCodable?
     public let extrasystemprompt: String?
     public let bootstrapcontextmode: AnyCodable?
@@ -514,6 +515,7 @@ public struct AgentParams: Codable, Sendable {
         cwd: String? = nil,
         cleanupbundlemcponrunend: Bool? = nil,
         modelrun: Bool? = nil,
+        modelrunoptions: [String: AnyCodable]? = nil,
         promptmode: AnyCodable? = nil,
         extrasystemprompt: String? = nil,
         bootstrapcontextmode: AnyCodable? = nil,
@@ -563,6 +565,7 @@ public struct AgentParams: Codable, Sendable {
         self.cwd = cwd
         self.cleanupbundlemcponrunend = cleanupbundlemcponrunend
         self.modelrun = modelrun
+        self.modelrunoptions = modelrunoptions
         self.promptmode = promptmode
         self.extrasystemprompt = extrasystemprompt
         self.bootstrapcontextmode = bootstrapcontextmode
@@ -614,6 +617,7 @@ public struct AgentParams: Codable, Sendable {
         case cwd
         case cleanupbundlemcponrunend = "cleanupBundleMcpOnRunEnd"
         case modelrun = "modelRun"
+        case modelrunoptions = "modelRunRequestedOverrides"
         case promptmode = "promptMode"
         case extrasystemprompt = "extraSystemPrompt"
         case bootstrapcontextmode = "bootstrapContextMode"

packages/gateway-protocol/src/schema/agent.ts
@@ -321,6 +321,12 @@ export const AgentParamsSchema = closedObject({
   // bundle MCP resources after the run instead of keeping them warm.
   cleanupBundleMcpOnRunEnd: Type.Optional(Type.Boolean()),
   modelRun: Type.Optional(Type.Boolean()),
+  modelRunRequestedOverrides: Type.Optional(
+    closedObject({
+      maxTokens: Type.Optional(Type.Integer({ minimum: 1, maximum: 1_000_000 })),
+      temperature: Type.Optional(Type.Number({ minimum: 0, maximum: 2 })),
+    }),
+  ),
   promptMode: Type.Optional(
     Type.Union([Type.Literal("full"), Type.Literal("minimal"), Type.Literal("none")]),
   ),

src/cli/capability-cli.test.ts
@@ -758,7 +758,7 @@ describe("capability cli", () => {
       messages?: Array<{ content?: unknown; role?: unknown }>;
       systemPrompt?: unknown;
     };
-    options?: { reasoning?: unknown };
+    options?: { maxTokens?: unknown; reasoning?: unknown; temperature?: unknown };
   };
   type ImageDescribeParams = {
     agentId?: string;
@@ -1400,6 +1400,159 @@ describe("capability cli", () => {
     await runCapability("model", "run", "--prompt", "hello", "--thinking", "high", "--json");
 
     expect(firstCompletionCall()?.options?.reasoning).toBe("high");
+  });
+
+  describe.runIf(process.platform !== "win32")("POSIX prompt files", () => {
+    it("reads private model prompts from a mode-0600 prompt file", async () => {
+    const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), "openclaw-model-run-prompt-"));
+    const promptFile = path.join(tempDir, "prompt.txt");
+    await fs.writeFile(promptFile, "private prompt", { mode: 0o600 });
+
+    try {
+      await runCap("capability", "model", "run", "--prompt-file", promptFile, "--json");
+
+      expect(firstCompletionCall()?.context?.messages?.[0]?.content).toBe("private prompt");
+    } finally {
+      await fs.rm(tempDir, { recursive: true, force: true });
+    }
+  });
+
+  it("rejects prompt text combined with a prompt file", async () => {
+    const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), "openclaw-model-run-prompt-"));
+    const promptFile = path.join(tempDir, "prompt.txt");
+    await fs.writeFile(promptFile, "private prompt", { mode: 0o600 });
+
+    try {
+      await expect(
+        runCap(
+          "capability",
+          "model",
+          "run",
+          "--prompt",
+          "argv prompt",
+          "--prompt-file",
+          promptFile,
+          "--json",
+        ),
+      ).rejects.toThrow("exit 1");
+
+      expectRuntimeErrorContains("Use exactly one of --prompt or --prompt-file.");
+      expect(mocks.completeWithPreparedSimpleCompletionModel).not.toHaveBeenCalled();
+      expect(mocks.callGateway).not.toHaveBeenCalled();
+    } finally {
+      await fs.rm(tempDir, { recursive: true, force: true });
+    }
+  });
+
+  it.each([
+    {
+      name: "group-readable file",
+      setup: async (tempDir: string) => {
+        const promptFile = path.join(tempDir, "prompt.txt");
+        await fs.writeFile(promptFile, "private prompt", { mode: 0o600 });
+        await fs.chmod(promptFile, 0o640);
+        return promptFile;
+      },
+      expected: "must have mode 0600",
+    },
+    {
+      name: "symbolic link",
+      setup: async (tempDir: string) => {
+        const target = path.join(tempDir, "target.txt");
+        const promptFile = path.join(tempDir, "prompt.txt");
+        await fs.writeFile(target, "private prompt", { mode: 0o600 });
+        await fs.symlink(target, promptFile);
+        return promptFile;
+      },
+      expected: "must not be a symbolic link",
+    },
+    {
+      name: "empty file",
+      setup: async (tempDir: string) => {
+        const promptFile = path.join(tempDir, "prompt.txt");
+        await fs.writeFile(promptFile, "", { mode: 0o600 });
+        return promptFile;
+      },
+      expected: "cannot be empty",
+    },
+    {
+      name: "oversized file",
+      setup: async (tempDir: string) => {
+        const promptFile = path.join(tempDir, "prompt.txt");
+        await fs.writeFile(promptFile, Buffer.alloc(1024 * 1024 + 1, 0x61), { mode: 0o600 });
+        return promptFile;
+      },
+      expected: "must not exceed 1048576 bytes",
+    },
+  ])("rejects an unsafe $name prompt input", async ({ setup, expected }) => {
+    const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), "openclaw-model-run-prompt-"));
+    try {
+      const promptFile = await setup(tempDir);
+      await expect(
+        runCap("capability", "model", "run", "--prompt-file", promptFile, "--json"),
+      ).rejects.toThrow("exit 1");
+
+      expectRuntimeErrorContains(expected);
+      expect(mocks.completeWithPreparedSimpleCompletionModel).not.toHaveBeenCalled();
+      expect(mocks.callGateway).not.toHaveBeenCalled();
+    } finally {
+      await fs.rm(tempDir, { recursive: true, force: true });
+    }
+  });
+
+  });
+
+  it.runIf(process.platform === "win32")(
+    "rejects prompt files on Windows before model execution",
+    async () => {
+      await expect(
+        runCap("capability", "model", "run", "--prompt-file", "prompt.txt", "--json"),
+      ).rejects.toThrow("exit 1");
+
+      expectRuntimeErrorContains("--prompt-file is supported only on POSIX hosts.");
+      expect(mocks.completeWithPreparedSimpleCompletionModel).not.toHaveBeenCalled();
+      expect(mocks.callGateway).not.toHaveBeenCalled();
+    },
+  );
+
+  it("applies and reports requested local model-run overrides", async () => {
+    await runCap(
+      "capability",
+      "model",
+      "run",
+      "--prompt",
+      "hello",
+      "--max-output-tokens",
+      "64",
+      "--temperature",
+      "0",
+      "--json",
+    );
+
+    expect(firstCompletionCall()?.options).toMatchObject({ maxTokens: 64, temperature: 0 });
+    expect(firstJsonOutput()?.requestedOverrides).toEqual({ maxOutputTokens: 64, temperature: 0 });
+  });
+
+  it("propagates and reports requested gateway model-run overrides", async () => {
+    await runCap(
+      "capability",
+      "model",
+      "run",
+      "--prompt",
+      "hello",
+      "--gateway",
+      "--max-output-tokens",
+      "64",
+      "--temperature",
+      "0",
+      "--json",
+    );
+
+    expect(firstGatewayCall()?.params?.modelRunRequestedOverrides).toEqual({
+      maxTokens: 64,
+      temperature: 0,
+    });
+    expect(firstJsonOutput()?.requestedOverrides).toEqual({ maxOutputTokens: 64, temperature: 0 });
   });
 
   it("passes image files to gateway model probes as attachments", async () => {

src/gateway/agent-turn/agent-request-preflight.ts
@@ -217,6 +217,14 @@ export function prepareAgentRequestPreflight(params: {
     ]);
     return undefined;
   }
+  if (request.modelRunRequestedOverrides !== undefined && !isOneShotModelRun) {
+    params.respond(
+      false,
+      undefined,
+      errorShape(ErrorCodes.INVALID_REQUEST, "modelRunRequestedOverrides requires modelRun=true."),
+    );
+    return undefined;
+  }
   if (requestedModelOverride && !allowModelOverride) {
     params.io.emitAcceptance([
       false,

src/gateway/server-methods/agent.events-and-subagents.test-utils.ts
@@ -485,6 +485,7 @@ describe("gateway agent handler", () => {
         provider: "ollama",
         model: "llama3.2:latest",
         modelRun: true,
+        modelRunRequestedOverrides: { maxTokens: 64, temperature: 0 },
         promptMode: "none",
         sessionKey: "agent:main:main",
         inputProvenance: {
@@ -503,6 +504,7 @@ describe("gateway agent handler", () => {
     const callArgs = await waitForAgentCommandCall<{
       message?: string;
       modelRun?: boolean;
+      streamParams?: { maxTokens?: number; temperature?: number };
       promptMode?: string;
     }>();
     expectRecordFields(callArgs, {
@@ -510,9 +512,32 @@ describe("gateway agent handler", () => {
       modelRun: true,
       promptMode: "none",
     });
+    expect(callArgs.streamParams).toEqual({ maxTokens: 64, temperature: 0 });
     expect(callArgs.message).not.toContain("[Inter-session message]");
 
     resetTimeConfig();
+  });
+
+  it("rejects model-run generation settings outside model-run requests", async () => {
+    primeMainAgentRun({ cfg: mocks.loadConfigReturn });
+    mocks.agentCommand.mockClear();
+
+    const respond = await invokeAgent(
+      {
+        message: "unsafe settings",
+        agentId: "main",
+        sessionKey: "agent:main:main",
+        modelRunRequestedOverrides: { maxTokens: 64, temperature: 0 },
+        idempotencyKey: "test-model-run-options-without-model-run",
+      },
+      { reqId: "model-run-options-without-model-run", flushDispatch: false },
+    );
+
+    expectRespondError(respond, {
+      code: ErrorCodes.INVALID_REQUEST,
+      message: "modelRunRequestedOverrides requires modelRun=true.",
+    });
+    expect(mocks.agentCommand).not.toHaveBeenCalled();
   });
 
   it("rejects promptMode none without the stateless model-run contract", async () => {