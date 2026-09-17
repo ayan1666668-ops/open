d4e17b954c09c6bf8cc3bda9e911f5e984dbd9ce
Peter Steinberger <steipete@gmail.com>
2026-09-17 12:24:32 -0700

    fix(update): remove the relocated test type import

--- src/gateway/server-methods/update.test.ts
+++ src/gateway/server-methods/update.test.ts
@@ -43,7 +43,6 @@ import {
   captureUpdateRunPayload,
   mockGlobalInstallSurface,
   mockGitInstallSurface,
-  type UpdateRunPayload,
 } from "./update.test-harness.js";
 
 function readCapturedPayload(): RestartSentinelPayload {
