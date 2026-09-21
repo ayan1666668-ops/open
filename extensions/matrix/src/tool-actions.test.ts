clawdbot/clawdbot

feat(matrix): support outbound emote messages

extensions/matrix/src/tool-actions.test.ts
@@ -423,6 +423,27 @@ describe("handleMatrixAction pollVote", () => {
     });
   });
 
+  it("forwards emote intent to Matrix message sends", async () => {
+    const cfg = { channels: { matrix: { actions: { messages: true } } } } as CoreConfig;
+
+    await handleMatrixAction(
+      {
+        action: "sendMessage",
+        accountId: "ops",
+        to: "room:!room:example",
+        content: "waves",
+        emote: true,
+      },
+      cfg,
+    );
+
+    expect(mocks.sendMatrixMessage.mock.lastCall?.[2]).toMatchObject({
+      cfg,
+      accountId: "ops",
+      emote: true,
+    });
+  });
+
   it.each(["sendMessage", "editMessage"] as const)(
     "preserves indented Markdown when handling %s",
     async (action) => {

ayan1666668-ops/open

extensions/matrix/src/tool-actions.test.ts
@@ -340,6 +340,26 @@ describe("Matrix public message actions", () => {
     });
   });
 
+  it("forwards emote intent to Matrix message sends", async () => {
+    const cfg = { channels: { matrix: { actions: { messages: true } } } } as CoreConfig;
+    await runMatrixAction(
+      "send",
+      {
+        to: "room:!room:example",
+        message: "waves",
+        emote: true,
+      },
+      cfg,
+      { accountId: "ops" },
+    );
+
+    expect(mocks.sendMatrixMessage.mock.lastCall?.[2]).toMatchObject({
+      cfg,
+      accountId: "ops",
+      emote: true,
+    });
+  });
+
   it.each([
     { action: "send", markdown: "    @room" },
     { action: "send", markdown: "    @alice:example.org" },